b467d78cedfd58e680b0b05f185bbd1cbe5a33a3
Emilio <22030128@itcelaya.edu.mx>
2026-05-26 21:39:37 -0600

    Cambio de rol en el endpoint /ventas/{id}/completar

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -255,7 +255,7 @@ class UsuarioController extends Controller
                     404
                 );
             }
-            $parts = explode('/public/usuarios/', $usuario->url);
+            $parts = explode('s3/', $usuario->url);
 
 
 
